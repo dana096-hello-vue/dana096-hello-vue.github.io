730ac3d8575ecba926f8791aa295342489f4e6db
dana096 <lovejiwon11@naver.com>
2024-01-31 17:21:19 +0900

    영화 목록과 정보 모두 출력하기
    - 모든 목록 출력한 이미지

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <script setup>
 import { ref } from 'vue'
-import category from "@/assets/category.json"
+import category from "@/assets/movie.json"
 const categories = ref(category)
 </script>
 
 <template>
-<ul>
-<li v-for="(item) in categories">
-	name : {{ item.name }} <br>
-	code : {{ item.code }}	
-</li>
-</ul>
+<h1 style="margin-bottom: 30px;">영화 목록</h1>
+  <ul>
+    <li v-for="movie in categories" class="movie-item">
+      <div>
+        <strong style="font-weight: bold;">Title:</strong> {{ movie.Title }} <br>
+        <strong style="font-weight: bold;">Runtime:</strong> {{ movie.Runtime }} <br>
+        <strong style="font-weight: bold;">Director:</strong> {{ movie.Director }} <br>
+        <strong style="font-weight: bold;">Year:</strong> {{ movie.Year }} <br>
+        <strong style="font-weight: bold;">Actors:</strong>
+        <ul>
+          <li v-for="actor in movie.Actors">
+            {{ actor.name }} - {{ actor.nationality }}
+          </li>
+        </ul>
+        <strong style="font-weight: bold;">Poster:</strong> {{ movie.Poster }} <br>
+        <strong style="font-weight: bold;">Festivals:</strong>
+	<ul>
+          <li v-for="festival in movie.Festivals" v-text="festival"></li>
+	</ul>
+      </div>
+    </li>
+  </ul>
 </template>
+
+<style scoped>
+.movie-item {
+  margin-bottom: 30px;
+}
+</style>
