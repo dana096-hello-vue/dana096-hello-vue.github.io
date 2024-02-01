87cd76ae985e19667c6dd4209d61a2dabdfd300f
dana096 <lovejiwon11@naver.com>
2024-02-01 10:28:47 +0900

    2.6 computed

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,21 @@
 <script setup>
-import { ref } from 'vue'
-import category from "@/assets/movie.json"
-const categories = ref(category)
-const keyword = ref(2014)
+import { reactive, computed, ref } from 'vue'
+
+const author = ref({
+name: 'John Doe',
+books: [1, 2, 3]
+})
+
+// a computed ref
+const publishedBooksMessage = computed(() => {
+return author.value.books.length > 2 ? 'Yes' : 'No' 
+})
 </script>
 
 <template>
-  <div>
-    <ul>
-      <h1 style="margin-bottom: 30px;">2014년 영화 목록</h1>
-      <li v-for="movie in categories" class="movie-item">
-        <div v-if="movie.Year =  2014">
-          <strong style="font-weight: bold;">Title:</strong> {{ movie.Title }} <br>
-          <strong style="font-weight: bold;">Runtime:</strong> {{ movie.Runtime }} <br>
-          <strong style="font-weight: bold;">Director:</strong> {{ movie.Director }} <br>
-          <strong style="font-weight: bold;">Year:</strong> {{ movie.Year }} <br>
-          <strong style="font-weight: bold;">Actors:</strong>
-          <ul>
-            <li v-for="actor in movie.Actors">
-              {{ actor.name }} - {{ actor.nationality }}
-            </li>
-          </ul>
-          <strong style="font-weight: bold;">Poster:</strong> {{ movie.Poster }} <br>
-          <strong style="font-weight: bold;">Festivals:</strong>
-          <ul>
-            <li v-for="festival in movie.Festivals" v-html="festival"></li>
-          </ul>
-        </div>
-      </li>
-    </ul>
-  </div>
+<span>😎: </span>
+<span>{{ author.books.length > 2 ? 'Yes' : 'No' }}</span>
+<div>computed ↓</div>
+<span>{{ publishedBooksMessage }}</span>
 </template>
-
-<style scoped>
-.movie-item {
-  margin-bottom: 30px;
-}
-</style>
 
