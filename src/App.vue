eeb98465f6df89378198dead17e7bee90d4e0eb1
dana096 <lovejiwon11@naver.com>
2024-02-01 17:25:43 +0900

    create TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <script setup>
-import { ref, onUpdated, onMounted, onBeforeUpdate } from 'vue'
-const count = ref(0)
-onUpdated(() => {
-	console.log('onUpdated', document.getElementById('count').textContent)
-})
-onMounted(() => {
-        console.log('onMounted', document.getElementById('count').textContent)
-})
-onBeforeUpdate(() => {
-        console.log('onBeforeUpdate', document.getElementById('count').textContent)
-})
+import { ref } from 'vue'
+
+let id = 0
+const newTodo = ref('')
+const todoes = ref([
+	{ id: id++, issue: 'Backlog' }, 
+	{ id: id++, issue: 'Todo' },
+	{ id: id++, issue: 'Go Hawaii' }
+])
+
+function addTodo() {
+	console.log('id:', id, ', issue:', newTodo.value);
+	todoes.value.push({ id: id++, issue: newTodo.value })
+	newTodo.value = ''
+}
+
+function removeTodo (todo) {
+        console.log(todo)	
+        todoes.value = todoes.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
-<button id="count" @click="count++">{{ count }}</button>
+	<form @submit.prevent ="addTodo">
+		<input v-model="newTodo">
+		<button>할 일 추가</button>
+	</form>
+	<ul>
+		<li v-for="todo in todoes">
+			{{ todo.issue }} - <button @click="removeTodo(todo)">Done</button>
+		</li>
+	</ul>
 </template>
