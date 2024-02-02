8a12d32dfb1441728b1e59ea041dcbf9cf1db908
dana096 <lovejiwon11@naver.com>
2024-02-02 11:10:36 +0900

    3.2 morning-gymnastics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
 import { ref } from 'vue'
-import Child from './components/Child.vue'
-const pocketMoney = ref('부모 컴포넌트로부터 <💌>을 전달받았어요!')
+
+const show = ref(true)
+const list = ref([1, 2, 3])
+
+function rendering() {
+	show.value = !show.value
+}
+
+function addList() {
+	list.value.push(list.value.length + 1)
+}
+
+function removeList() {
+	list.value.pop()
+}
+
+function reverse() {
+	list.value.reverse()
+}
 </script>
 
 <template>
-<Child :pocketMoney="pocketMoney" />
+<button @click="rendering()">List 렌더링 ON/OFF </button>
+<button @click="addList()">List 추가</button>
+<button @click="removeList()">List 제거</button>
+<button @click="reverse()">List 뒤집기</button>
+
+<ul v-if="show && list.length">
+	<li v-for="item of list">{{ item }}</li>
+</ul>
+<p v-else-if="list.length">List is not empty, but hidden.</p>
+<p v-else>List is emtpy.</p>
 </template>
